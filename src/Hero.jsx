e85ef4627a1179d9018428f65798064f9b0f7ab9
Mahendra <111168963+Mah3n@users.noreply.github.com>
2025-08-01 11:15:57 +0700

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
     <div className='h-[100dvh] w-full flex items-center justify-center relative'>
         <div className="home-wrapper max-w-6xl">
             <h1 className='text-2xl md:text-6xl text-white font-bold mb-1 md:mb-4 text-wrap'>Hello, my name is <span className='text-[#3457D5]'>Mahendra.</span></h1>
-            <h2 className='text-base md:text-3xl text-white font-medium inline-block px-2 bg-[#3457D5]'>Im a Front-end Web Developer</h2>
+            <h2 className='text-base md:text-3xl text-white font-medium inline-block px-2 bg-[#3457D5]'>Im a Web Developer</h2>
             <p className="introduction text-white md:mt-4 mt-2 mb-4 text-justify md:max-w-[50rem] max-w-[20rem] md:text-md">
             I'm a final-year student at the Islamic University of Balitar with a strong passion for web development. 
             I've built several projects, including a library management system, an employee payroll app, and a course recommendation system. 
